9ed3711daf6b5f81d1f3c86227728fa76f9ad661
Johan Svensson <partlight.johans@gmail.co>
2018-11-30 12:38:55 +0100

    bmc card html sanitizing

--- src/index.ts
+++ src/index.ts
@@ -47,5 +47,4 @@ setupDb().then(() => {
   app.post(`${basePath}/enterprise/:enterpriseId/member`, require('./post/member').default);
   app.delete(`${basePath}/enterprise/:enterpriseId/member/:targetEmail`, require('./delete/member').default);
   app.put(`${basePath}/enterprises/:enterpriseId/bmc/:cardId`, require('./put/bmc_card').default);
-  app.delete(`${basePath}/enterprises/:enterpriseId/bmc/:cardId`, require('./put/bmc_card').default);
 })
--- src/put/bmc_card.ts
+++ src/put/bmc_card.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
 import { query } from "../db";
 import { requireRoleWithResponse } from "../auth/role_auth";
+import sanitizeHTML from 'sanitize-html';
 
 let updateableColumns = [
-  
-]
+  'content'
+];
+
+//  Allowed HTML tags in the card content
+let allowedContentTags = [
+  'a', 'img', 'p', 'h2', 'em', 'u', 'strong'
+];
 
 /**
  * Updates an existing business model canvas card.
@@ -15,16 +21,48 @@ export default async (req: Request, res: Response) => {
     return;
   }
 
-  let { params } = req;
+  let { params, body } = req;
+
+  //  Columns to update in this request
+  let columnsToUpdate = {};
+
+  for (let col of updateableColumns) {
+    if (col in body) {
+      columnsToUpdate[col] = body[col];
+    }
+
+    if (col == "content") {
+      //  Sanitize content
+      columnsToUpdate[col] = sanitizeHTML(columnsToUpdate[col], {
+        allowedTags: allowedContentTags,
+        allowedAttributes: {
+          'a': ['href']
+        },
+        allowedIframeHostnames: ['www.youtube.com']
+      })
+    }
+  }
+  let columnsToUpdateKeys = Object.keys(columnsToUpdate);
+  if (!columnsToUpdateKeys.length) {
+    return res.status(400).end(JSON.stringify({
+      result: 'error',
+      error: 'noUpdate'
+    }));
+  }
 
   let enterpriseId: string = params.enterpriseId;
+  let cardId: string = params.cardId;
 
   let result = await query(
-    `UPDATE card SET content="why why"
-      WHERE card.enterprise_id = (
-          SELECT id FROM enterprise WHERE public_id = ?
-      )`,
-    [enterpriseId]
+    `UPDATE card SET
+      ${columnsToUpdateKeys.map(col => `${col}=?`)}
+      JOIN enterprise ON enterprise.id = card.enterprise_id
+      WHERE card.id = ? AND enterprise.public_id = ?`,
+    [
+      ...(columnsToUpdateKeys.map(col => columnsToUpdate[col])),
+      cardId,
+      enterpriseId
+    ]
   ) as any;
 
   console.log("Result: ", result);
